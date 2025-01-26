00de099c1faa6c48d8329c4485897b89ba16f33c
Ivan-Alexandar2 <ivan.alexander.steafanov@gmail.com>
2025-01-26 20:10:47 +0200

    Login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -60,19 +60,20 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+            <div class="card bg-dark text-white"> <!-- Черен фон за картата -->
+                <div class="card-header bg-orange text-white"> <!-- Оранжев фон за хедъра -->
+                    <h3 class="mb-0">{{ __('Login') }}</h3>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
+                        <!-- Имейл -->
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
+                            <label for="email" class="col-md-4 col-form-label text-md-end text-orange">{{ __('Email Address') }}</label> <!-- Оранжев текст -->
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                                <input id="email" type="email" class="form-control bg-dark text-white @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -81,12 +82,11 @@
                             </div>
                         </div>
 
+                        <!-- Парола -->
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
+                            <label for="password" class="col-md-4 col-form-label text-md-end text-orange">{{ __('Password') }}</label> <!-- Оранжев текст -->
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                                <input id="password" type="password" class="form-control bg-dark text-white @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -95,26 +95,27 @@
                             </div>
                         </div>
 
+                        <!-- Запомни ме -->
                         <div class="row mb-3">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
+                                    <label class="form-check-label text-orange" for="remember"> <!-- Оранжев текст -->
                                         {{ __('Remember Me') }}
                                     </label>
                                 </div>
                             </div>
                         </div>
 
+                        <!-- Бутон за вход -->
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-orange text-white"> <!-- Оранжев бутон -->
                                     {{ __('Login') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    <a class="btn btn-link text-orange" href="{{ route('password.request') }}"> <!-- Оранжев текст -->
                                         {{ __('Forgot Your Password?') }}
                                     </a>
                                 @endif
